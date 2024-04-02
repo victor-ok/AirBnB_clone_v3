2aa21942b8508baca35bf76e026d64b8b6e0f45e
victor-ok <vua.okafor@gmail.com>
2024-04-02 01:00:25 +0100

    clone_v3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,6 +63,22 @@ class FileStorage:
         except (AttributeError, KeyError):
             pass
 
-def close(self):
+    def close(self):
         """Call the reload method."""
         self.reload()
+
+    def get(self, cls, id):
+            """
+                retrieves one object based on class name and id
+            """
+            if cls and id:
+                fetch_obj = "{}.{}".format(cls, id)
+                all_obj = self.all(cls)
+                return all_obj.get(fetch_obj)
+            return None
+
+    def count(self, cls=None):
+        """
+        count of all objects in storage
+        """
+        return (len(self.all(cls)))
--- models/user.py
+++ models/user.py
@@ -1,30 +1,49 @@
 #!/usr/bin/python3
-"""defines a user class"""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+"""
+User Class from Models Module
+"""
+import hashlib
+import os
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
-    a user that inherits from the BaseModel
-
-    attr:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
+        User class handles all application users
     """
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        """
+            instantiates user object
+        """
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                User.__set_password(self, pwd)
+        super().__init__(*args, **kwargs)
+
+    def __set_password(self, pwd):
+        """
+            custom setter: encrypts password to MD5
+        """
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        setattr(self, "password", secure_password)
